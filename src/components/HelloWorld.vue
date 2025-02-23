cd649418c4a352d73952505e75323b834002bd4a
Simon25772 <ssh.king@qq.com>
2025-02-24 03:25:55 +0800

    perfect ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,37 +3,65 @@
     height: 100vh;
     background-color: #eee;" class="flex">
       <!-- 侧边栏 -->
-      <div style="width: 15vw;" class="baiSe">
+      <div style="width: 15vw;background-color: rgb(247,248,252);font-family: Roboto;" class="felx">
           <!-- 工具栏 -->
           <div style="padding: 10px;
           align-items: center;
           justify-content: center;
-          border-bottom:1px solid #999;" class="flex">
+          border-bottom:1px solid #999;
+          height: 5vh;" class="flex">
             <!-- 新建按钮 -->
-            <el-button type="primary" icon="el-icon-edit" circle @click="createSession"></el-button>
+            <el-button icon="el-icon-plus" @click="createSession" style="width: 12vw;">新建对话</el-button>
           </div>
-          <div class="huiHua" v-for="(item,index) in sessions"  :key="index" @click="checkoutSession(index)">{{ index }}:{{ item.title }}</div>
+            <div class="huiHua flex row" v-for="(item,index) in sessions"  :key="index" @click="checkoutSession(index)">
+              <div class="flex1" style="text-align: left;">
+                {{ index }}:{{ item.title }}
+              </div>
+              <div>
+                <i class="el-icon-delete"></i>
+              </div>
+            </div>
       </div>
       <!-- 主区域 -->
-      <div class="flex flex1 column" >
+      <div class="flex flex1 column baiSe" >
           <!-- 头部栏 -->
-          <div style="height: 7vh;" class="baiSe"></div>
+          <div style="height: 7vh;justify-content: space-between;" class="baiSe flex">
+            <div style="margin: 20px;font-size: 20px;font-family: Fantasy;">Advanced-LM</div>
+            <i class="el-icon-user-solid" style="font-size:30px;margin: 20px;"></i>
+          </div>
           <!-- 内容区 -->
-          <!-- <div class="flex1" style="white-space: pre-wrap;">{{chatContent}}</div> -->
-          <div class="flex flex1" style="white-space: pre-wrap;overflow: auto;">
+          <div class="flex flex1" style="white-space: pre-wrap;overflow: auto;font-family: Inter;">
             <div v-if="sessions.length>0" class="flex1">
               <div class="xiaoXi flex row" v-for="(item,index) in sessions[currentSessionIndex].history" :key="index">
-                <div class="flex1">
-                  {{ item.role }}:{{ item.content }}
+                <div v-if="item.role=='User'" class="flex row">
+                  <el-image 
+                    style="width: 40px;height:40px;border-radius: 50%;"
+                    :src="userUrl" 
+                    :preview-src-list="srcList">
+                  </el-image>
+                  <div class="flex1" style="width: 77vw;">
+                  {{ item.role }}:<br>{{ item.content }}
+                  </div>
+                  <i class="el-icon-edit" @click="rollback(index)"></i>
                 </div>
-                <div>回滚</div>
+                <div v-else class="flex row">
+                  <el-image 
+                    style="width: 40px;height:40px;border-radius: 50%;"
+                    :src="aiUrl" 
+                    :preview-src-list="srcList">
+                  </el-image>
+                  <div class="flex1" style="width: 77vw;">
+                  {{ item.role }}:<br>{{ item.content }}
+                  </div>
+                    <i class="el-icon-refresh-right"></i>
+                </div>
               </div>
             </div>
           </div>
           <!--底部栏-->
           <div style="height: 10vh; justify-self: end;justify-content: center;" class="baiSe flex row">
             <!-- 输入框 -->
-            <div style="width: 700px;">
+            <div style="width: 65vw;">
               <el-input
                 type="textarea"
                 placeholder="给GPT发送消息"
@@ -44,8 +72,9 @@
             </div>
             <!-- 提交表单按钮 -->
             <div style="justify-content: center;" class="flex column">
-              <el-button v-if="textarea.length>0&&!isGenerating" @click="sendMessage" type="primary" icon="el-icon-top" circle></el-button>
-              <el-button v-else icon="el-icon-top" circle disabled></el-button>
+              <el-button v-if="sessions.length>currentSessionIndex&&textarea.length>0&&!sessions[currentSessionIndex].isGenerating" @click="sendMessage" type="primary" icon="el-icon-top" circle></el-button>
+              <el-button v-else-if="sessions.length>currentSessionIndex&&sessions[currentSessionIndex].isGenerating" icon="el-icon-loading" @click="stopGenerate" circle type="primary"></el-button>
+              <el-button v-else icon="el-icon-top" circle :disabled="true"></el-button>
             </div>
           </div>
       </div>
@@ -54,6 +83,7 @@
 
 <script>
 import axios from "axios";
+import Vue from 'vue';
 
 export default {
   name: 'HelloWorld',
@@ -62,6 +92,8 @@ export default {
   },
   data () {
     return {
+      aiUrl: 'https://chat.qwenlm.ai/static/favicon.png',
+      userUrl: 'https://cdn.qwenlm.ai/output/64b596f9-e26b-4ba2-a3c0-681a6f1d3df9/t2i/a49fed19-13a9-4dae-8d2e-7492e3ae5a5e/66fbbb3d-d8c1-47d7-85bd-2347da84549b.png',
       count: 10,
       loading: false,
       textarea:"",
@@ -72,14 +104,44 @@ export default {
     }
   },
   methods:{
+    rollback(index){
+      const id=this.sessions[this.currentSessionIndex].id;
+      this.textarea=this.sessions[this.currentSessionIndex].history[index].content;
+      axios
+      .get("http://localhost:8081/rollback", {
+        params:{
+          id:id,
+          index:index
+        }
+      })
+      .then((response) => {
+        console.log("rollback");
+        this.sessions = response.data;
+      })
+    },
+    stopGenerate(){
+      const index=this.currentSessionIndex;
+      if (this.sessions[index].controller) {
+        this.sessions[index].controller.abort(); // 中断请求
+        this.sessions[index].controller = null; // 清理控制器
+        Vue.set(this.sessions, index, { 
+          ...this.sessions[index], 
+          isGenerating: false 
+        });
+      }else{
+        console.log("中断控制器为空!");
+      }
+    },
     checkoutSession(index){
       this.currentSessionIndex=index;
     },
     // 创建新会话
     createSession(){
+      console.log("A")
       axios
       .get("http://localhost:8081/createSession")
       .then((response) => {
+        console.log("B")
         this.sessions = response.data;
       })
       .catch((error) => {
@@ -100,10 +162,12 @@ export default {
     },
     
     async fetchStream() {
-      this.isGenerating=true;
       const id=this.sessions[this.currentSessionIndex].id;
       const content=this.textarea;
       const myIndex=this.currentSessionIndex;
+      this.sessions[myIndex].isGenerating=true;
+      this.sessions[myIndex].controller=new AbortController();
+      const signal = this.sessions[myIndex].controller.signal;
       // 处理流式响应
       try {
         const response = await fetch(`http://localhost:8081/chat?id=${id}&content=${encodeURIComponent(content)}`, {
@@ -111,6 +175,7 @@ export default {
             headers: {
                 'Content-Type': 'application/json',
             },
+            signal,
         });
         const reader = response.body.getReader();
         const decoder = new TextDecoder();
@@ -128,9 +193,8 @@ export default {
 
           // 解码并拼接分块数据
           this.sessions[myIndex].history.at(-1).content += decoder.decode(value, { stream: true });
-          this.chatContent += decoder.decode(value, { stream: true });
         }
-        this.isGenerating=false;
+        this.sessions[myIndex].isGenerating=false;
       } catch (error) {
         console.error("Error during fetch or stream processing:", error);
       }
@@ -152,9 +216,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-*{
-    border: 1px solid black;
-}
+/* *{
+  border: solid 1px black;
+} */
 .flex{
     display: flex;
 }
@@ -172,11 +236,12 @@ export default {
 }
 .huiHua{
     padding: 10px 20px;
-    border-bottom:1px solid;
+    font-size: 14px;
+    /* border-bottom:1px solid; */
 }
 .xiaoXi{
     padding: 10px 20px;
-    border-bottom:1px solid;
+    /* border-bottom:1px solid; */
     text-align: left;
 }
 </style>
